9629116d72155bdd64f00ccd2d5e96025099b30b
Jean-François Lépine <lepinejeanfrancois@gmail.com>
2023-10-23 06:38:39 +0200

    wip

--- src/Analyzer/AstAnalyzer.go
+++ src/Analyzer/AstAnalyzer.go
@@ -6,11 +6,13 @@ import (
     "os"
     "io/ioutil"
     "log"
+    "fmt"
     "strconv"
     "sync"
     "github.com/golang/protobuf/proto"
     "github.com/pterm/pterm"
     "github.com/yargevad/filepathx"
+    "reflect"
 )
 
 func Start(progressbar *pterm.SpinnerPrinter) {
@@ -81,4 +83,66 @@ func executeFileAnalysis(file string) {
     // pbFile is as NodeType.File struct
     // We can calculate metrics on it
     // fmt.Println(pbFile.Path)
+
+    root := &ASTNode{children: pbFile.Stmts}
+    cyclomaticVisitor := &ComplexityVisitor{}
+    root.Accept(cyclomaticVisitor)
+
+    root.Visit()
+    complexity := cyclomaticVisitor.GetComplexity()
+
+    if complexity > 0 {
+    fmt.Println("La complexité cyclomatique de " + pbFile.Path + " est de " + strconv.Itoa(complexity))
+    }
+}
+
+type Visitor interface {
+    Visit(node *ASTNode)
+}
+
+type ComplexityVisitor struct {
+    complexity int
+}
+func (v *ComplexityVisitor) Visit(node *ASTNode) {
+    for _, child := range node.children.Stmts {
+        // if node.children has attribute "Stmts", it's a node with children. Iterate over it
+        // check if attribute "Stmts" exists, using reflect
+        fmt.Println(child.String())
+        dynamicStmts := reflect.ValueOf(child).Elem().FieldByName("Stmts")
+        fmt.Println(dynamicStmts)
+        if dynamicStmts != (reflect.Value{}) {
+            childNode := &ASTNode{children: dynamicStmts.Interface().(*pb.Stmts)}
+            childNode.Accept(v)
+
+            fmt.Println(child)
+            v.Visit(childNode)
+        } else {
+
+            // if type is pb.StmtDecisionIf, using dynamicStmts
+            if child.GetStmtDecisionIf() != nil {
+                v.complexity++
+            }
+        }
+
+    }
+}
+func (v *ComplexityVisitor) GetComplexity() int {
+	return v.complexity
+}
+
+
+// Structure de l'arbre AST
+type ASTNode struct {
+	children *pb.Stmts
+	Visitors []Visitor
+}
+
+func (n *ASTNode) Accept(visitor Visitor) {
+    n.Visitors = append(n.Visitors, visitor)
+}
+
+func (n *ASTNode) Visit() {
+	for _, v := range n.Visitors {
+        v.Visit(n)
+    }
 }
